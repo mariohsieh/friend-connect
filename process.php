3853fff39adca33a7a6c875f1b2c027bc64142a8
Mario Hsieh <mariohsieh@gmail.com>
2013-12-05 19:52:40 -0800

    backend validation, css changes

--- process.php
+++ process.php
@@ -8,6 +8,7 @@ class Process
 {
 	// declare variables
 	var $connection;
+	var $alerts = array();
 
 	// constructor, initializes $connection and calls the function to list countries or display info
 	public function __construct()
@@ -45,16 +46,76 @@ class Process
 
 	private function register()
 	{
-		// check to see if email address has already been registered
-		//$query = "SELECT * FROM users WHERE email = '{$remail}'";
-		//$users = $this->connection->fetch_all($query);
+		// back-end validation checks
 
-		// Register new user
-		$query = "INSERT INTO users (username, email, password, created_at) VALUES ('{$_POST['ruser']}', '{$_POST['remail']}', '".md5($_POST['rpass'])."', NOW())";
-		mysql_query($query);
+		// set variables
+		$user = $this->checkInput($_POST['ruser']);
+		$email = $this->checkInput($_POST['remail']);
+		$pass = $_POST['rpass'];
+		$confirm = $_POST['rpassconf'];
 
-		// Go back to login page
-		header("Location: index.php");
+		// check user name
+		if (!isset($user) || $user == "" || !preg_match("/^[a-zA-Z]*$/", $user))
+		{
+			$this->alerts['userError'] = "Please input a valid username.";
+		}
+
+		// check email addy
+		if (!isset($email) || $email == "" || !filter_var($email, FILTER_VALIDATE_EMAIL))	
+		{
+			$this->alerts['emailError'] = "Please input a valid email address.";
+		}
+
+		// check password
+		if (!isset($pass) || $pass == "" || strlen($pass)<8)
+		{
+			$this->alerts['passError'] = "Please input a valid password.";
+			if (!isset($confirm) || $confirm == "" || strlen($confirm)<8 || $confirm != $pass)
+				$this->alerts['confirmError'] = "Please confirm your password.";
+		}
+
+		// check for any errors
+		//if (count($alerts)>0)
+			//$_SESSION['alerts'] = $alerts;
+		//else
+		if (count($this->alerts) == 0)
+		{		
+			// check to see if email address has already been registered
+			$query = "SELECT * FROM users WHERE email = '{$email}'";
+			$users = $this->connection->fetch_all($query);
+
+			if (count($users)>0)
+			{
+				$this->alerts['emailError'] = "Your email is already on record.  Please login.";
+				//$_SESSION['alerts'] = $alerts;	
+			}			
+			else {
+				// Register new user
+				$query = "INSERT INTO users (username, email, password, created_at) VALUES ('{$user}', '{$email}', '".md5($pass)."', NOW())";
+				mysql_query($query);
+
+				$this->alerts['success'] = "Thank you for registering.  Please login now!";
+				//$_SESSION['alerts'] = $alerts;
+			}
+		}
+
+		//var_dump($this->alerts);
+		//die();
+
+		// send data to frontend using json
+		echo json_encode($this->alerts);
+
+		// Go back to sign-in page
+		//header("Location: index.php");
+	}
+
+	// checks input text
+	private function checkInput($item) 
+	{
+		$item = trim($item);
+		$item = stripslashes($item);
+		$item = htmlspecialchars($item);
+		return $item;
 	}
 }
 
